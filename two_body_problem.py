6342b619be605dcecaf288d581207ad829c2cf04
Apoorva Singh <apoorvasingh@Apoorvas-MacBook-Pro.local>
2023-04-04 21:42:02 +0530

    modify: Fix spellings and add function for plotting about CoM
    This change fixes spellings and plots the two body system about
    the centre of mass.

--- two_body_problem.py
+++ two_body_problem.py
@@ -21,7 +21,7 @@ G = 6.67408e-20  # Universal Gravitation Constant (Units: (km^3) / (kg*(s^2)))
 time = np.arange(0, 480, 0.5) # time array
 
 
-def two_body_eqm(y, t, m1, m2):
+def two_body_eqn(y, t, m1, m2):
     r_norm_cubed = np.power(np.linalg.norm(y[3:6] - y[:3]), 3)
     c0 = y[6:12]
     c1 = G * m2 * ((y[3:6] - y[:3]) / r_norm_cubed)
@@ -87,6 +87,39 @@ def plot_per_frame_about_m1(idx, position_vectors):
     ax.set_zlim3d(XYZlim)
 
 
+def plot_per_frame_about_g(idx, position_vectors):
+    ax.clear()
+    r1, r2, g = position_vectors[:idx+1, :3], position_vectors[:idx+1, 3:6], position_vectors[:idx+1, 12:15]
+    r2 = r2 - g
+    r1 = r1 - g
+    g = g - g
+
+    # Plotting m1 (in red)
+    ax.plot3D(r1[:idx+1, 0], r1[:idx+1, 1], r1[:idx+1, 2], 'red')
+    ax.scatter(r1[idx, 0], r1[idx, 1], r1[idx, 2], c='red', marker='o')
+
+    # Plotting m2 (in blue)
+    ax.plot3D(r2[:idx+1, 0], r2[:idx+1, 1], r2[:idx+1, 2], 'blue')
+    ax.scatter(r2[idx, 0], r2[idx, 1], r2[idx, 2], c='blue', marker='o')
+
+    # Plotting G, the centre of mass (in black)
+    ax.plot3D(g[:idx+1, 0], g[:idx+1, 1], g[:idx+1, 2], 'black')
+    ax.scatter(g[idx, 0], g[idx, 1], g[idx, 2], c='black', marker='o')
+
+    ax.set_xlabel('X [km]')
+    ax.set_ylabel('Y [km]')
+    ax.set_zlabel('Z [km]')
+
+    # # Make axes limits
+    xyzlim = np.array([ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()]).T
+    XYZlim = np.asarray([min(xyzlim[0]), max(xyzlim[1])])
+    ax.set_title('Orbits of m1 and m2 about CoM \nTime = ' + str(np.round(time[idx], decimals=2)) + ' sec')
+    ax.set_xlim3d(XYZlim)
+    ax.set_ylim3d(XYZlim)
+    ax.set_zlim3d(XYZlim)
+
+
+
 def generate_positions():
     position_vectors = []
     # body m1 initial conditions
@@ -99,7 +132,7 @@ def generate_positions():
     # [X1, Y1, Z1, X2, Y2, Z2, VX1, VY1, VZ1, VX2, VY2, VZ2]
     # [(0) (1) (2) (3) (4) (5)  (6)  (7)  (8)  (9) (10) (11)]
     y0 = np.concatenate((r10, r20, v10, v20))
-    y = odeint(two_body_eqm, y0, time, args=(m1, m2))
+    y = odeint(two_body_eqn, y0, time, args=(m1, m2))
     for y_k in y:
         r1, r2 = y_k[:3], y_k[3:6]  # Positions of m1 and m2
         rg = ((m1 * r1) + (m2 * r2)) / (m1 + m2)  # Calculation for G, the centre of mass
@@ -125,4 +158,10 @@ if __name__ == "__main__":
     fig = plt.figure()
     ax = plt.axes(projection='3d')
     line_ani = animation.FuncAnimation(fig, plot_per_frame_about_m1, interval=30, frames=len(time), fargs=(position_vectors,), repeat=False)
+    plt.show()
+
+    ## Plotting m1 and m2 about CoM
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    line_ani = animation.FuncAnimation(fig, plot_per_frame_about_g, interval=30, frames=len(time), fargs=(position_vectors,), repeat=False)
     plt.show()
